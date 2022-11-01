eba652320ff52b76014a43550dc065cba72bf61b
Midzi Ribery <muhd.termidzi@gmail.com>
2022-11-01 22:58:40 +0800

    NavBar Start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <h1>Test Clean Slate</h1>
+      <NavBar/>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function NavBar() {
+    return(
+        <div>
+            <nav className="navbar">
+                <div className="navbar__container">
+                    <a>Test</a>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar;
