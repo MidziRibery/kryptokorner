688e386a48d540bbd3c5d19ac17991dd59d148d4
Midzi Ribery <muhd.termidzi@gmail.com>
2022-11-02 21:12:07 +0800

    recovered something wrong

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import NavBar from './components/NavBar';
 
+
 function App() {
   return (
     <div className="App">
       <h1>Test Clean Slate</h1>
-      <NavBar/>
+      <NavBar/>  
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,20 @@
 import React from "react";
+import './NavBar.css';
 
 function NavBar() {
     return(
         <div>
             <nav className="navbar">
                 <div className="navbar__container">
-                    <a>Test</a>
+                    <a className="brand">Krypto Korner </a><i className="logo">put img here</i>
+                </div>
+                <div className="navbar__links">
+                    <ul className="navbar__links__menu">
+                        <li>Home</li>
+                        <li>NFTS</li>
+                        <li>KryptoKnowledge</li>
+                        <li>Subscribe</li>
+                    </ul>
                 </div>
             </nav>
         </div>
