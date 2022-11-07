684b3d30c667fe68fb69df400bb9e7cfccd33106
Midzi Ribery <muhd.termidzi@gmail.com>
2022-11-07 19:47:13 +0800

    NFT pages

---
+++ src/components/NFTCards.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import CardItem from './CardItem'
+import './Cards.css';
+
+
+function NFTCards(props) {
+  return (
+    <div className='cards' >
+        <h1>Types of NFTS</h1>
+        <div className='cards__container'>
+            <div className='cards__wrapper'>
+                <ul className='cards__item'>
+                    <CardItem 
+                    src='https://img.seadn.io/files/7e10d61f5202d9c7cd7634eb495d31ff.png?fit=max&w=1000'
+                    text='Bored Ape Yatch Club (BAYC)'
+                    label='BAYC'
+                    path='/bitcoin'
+                    />
+                    <CardItem 
+                    src='https://images.jpgstoreapis.com/QmP1quFyKADvAm31ntMCpNYN5fUbpKg1FK7XGRSXtB5jNi600x'
+                    text='The first interactive NFT, exclusive on the Cardano Chain.'
+                    label='Dead PXLZ'
+                    path='/ethereum'
+                    />
+                    <CardItem 
+                    src='https://i.seadn.io/gae/IApNrbuWTJ1H0ll346fecNEMbqGBVIk6W0xNsArJslPJfaHDHeQlZCXMoaHQ0FSGHDYTqKzxmDbVC4wYI8uGy54jA0XBKrvtEskeJuo?auto=format&w=1000'
+                    text='Welcome to the Metaverse! Where everyone can afford a plot of land!'
+                    label='SAND LAND'
+                    path='/cardano'
+                    />
+                    <CardItem 
+                    src='https://img.seadn.io/files/2f5b8c9a38658fe6be20849fdc9940a9.jpg?fit=max&h=1200&w=1200&auto=format'
+                    text='An NFT for members only, for exclusive events'
+                    label='JRNY Club'
+                    path='/doge'
+                    />
+                    <CardItem 
+                    src='https://ipfs.poolpm.nftcdn.io/ipfs/QmdJvE44WMH8mdavKGZZfDungYFY3y8vvwbyHRHaixEMcv?ts=1667433600&exp=1209600&tk=boYNHwGwKJpl8-J4HpWqlrSgIGsn2Ywjol1vS8N9Wq8'
+                    text='When Snoop Dogg and Good Charlotte combine'
+                    label='Clay Nation'
+                    path='/usdt'
+                    />
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default NFTCards;
--- src/components/pages/NFT.js
+++ src/components/pages/NFT.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import '../../App.css';
 import Cards from '../Cards';
+import NFTCards from '../NFTCards';
 
 function NFT() {
   return (
     <>
         <h1 className='nft'>NFTs</h1>
-        <Cards/>
+        <NFTCards/>
     </>
 
   )
