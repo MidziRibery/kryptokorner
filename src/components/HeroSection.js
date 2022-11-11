3abc4c1716b2248e9e5c5b1554c58e4a7319b03c
Midzi Ribery <muhd.termidzi@gmail.com>
2022-11-11 21:41:42 +0800

    added video to herosection

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,10 +2,12 @@ import React from 'react'
 import '../App.css';
 import Button from './Button';
 import './HeroSection.css';
+import Video from '../videos/btcHeroSection.mp4'
 
 function HeroSection() {
   return (
     <div className='hero-container'>
+        <video src={Video} autoPlay loop muted/>
         <h1>KRYPTO KORNER</h1>
         <p>Your One-Stop Krypto Knowledge</p>
     </div>
