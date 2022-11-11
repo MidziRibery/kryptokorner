ce4f044d44907173831266364ed80b165b5b9073
Midzi Ribery <muhd.termidzi@gmail.com>
2022-11-11 23:19:38 +0800

    End of last day of touch up

--- src/components/pages/Bitcoin.js
+++ src/components/pages/Bitcoin.js
@@ -21,12 +21,12 @@ function Bitcoin() {
        console.log(res);
        const json = await res.json() // to get the object in API
        console.log('this is the data')
-       console.log(json.data) // here we get the joke
+       console.log(json.data) // here we get the json file
        console.log('this is my attempt to get the price')
        console.log(json.data[0].quote.USD.price)
        let oriPrice = json.data[0].quote.USD.price
        let price = parseFloat(oriPrice.toFixed(2));
-       setCurrPrice(price); // use state to set the currJoke
+       setCurrPrice(price); // use state to set the currPrice
     }
 
     useEffect( ()=>{
@@ -36,6 +36,7 @@ function Bitcoin() {
 
   return (
     <div>
+        <img src='https://thumbor.forbes.com/thumbor/fit-in/x/https://www.forbes.com/advisor/in/wp-content/uploads/2022/03/pexels-pixabay-315788-scaled.jpg'/>
         <h1>Bitcoin</h1>
         <h1>Current Price: ${currPrice}</h1>
     </div>
--- src/components/pages/NFT.js
+++ src/components/pages/NFT.js
@@ -3,6 +3,7 @@ import '../../App.css';
 import Cards from '../Cards';
 import DisplayCard from '../DisplayCard';
 import NFTCards from '../NFTCards';
+import './NFT.css';
 
 function NFT() {
 
@@ -13,17 +14,20 @@ function NFT() {
   }
   return (
     <>
-        <h1 className='nft'>NFTs</h1>
+      <div>
+        <img src='https://as2.ftcdn.net/v2/jpg/04/31/41/91/1000_F_431419169_GYVhYc9zOLG0FtcvOBlTeskGowecbHSi.jpg'/>
+      </div>
+      <div>
         <NFTCards cardsOnClick={handleCardOnClick}/>
-        {/* here we pass the 'remote control' aka setSelectedCard function (to
-        set the state of selectedCard) down to NFTCards.js. We wrap it in handleeCardOnClick
-        function, and send down the function as a prop of cardsOnClick.*/}
-        <DisplayCard card={selectedCard}/> 
-        {/* this one means to pass the prop cards down
-        to DisplayCard.js. But what is card? It is the selected
-        card, which is controlled by handleCardOnClick function which
-        sets the state of the card using setSelectedCard */}
-
+          {/* here we pass the 'remote control' aka setSelectedCard function (to
+          set the state of selectedCard) down to NFTCards.js. We wrap it in handleeCardOnClick
+          function, and send down the function as a prop of cardsOnClick.*/}
+          <DisplayCard card={selectedCard}/> 
+          {/* this one means to pass the prop cards down
+          to DisplayCard.js. But what is card? It is the selected
+          card, which is controlled by handleCardOnClick function which
+          sets the state of the card using setSelectedCard */}
+      </div>
     </>
 
   )
